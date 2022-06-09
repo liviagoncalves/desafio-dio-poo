f5f1dbbdf1179ac1d2c32eac0261ad7e2e78befc
liviagoncalves <liviagoncalves.morais@gmail.com>
2022-06-08 21:15:54 -0300

    Abstração e encapsulamento

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+import br.com.lgm.dominio.Curso;
+import br.com.lgm.dominio.Mentoria;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        Curso curso1 = new Curso();
+
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("Descrição curso java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+
+        curso2.setTitulo("Curso Js");
+        curso2.setDescricao("Descrição curso js");
+        curso2.setCargaHoraria(4);
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+
+        Mentoria mentoria = new Mentoria();
+
+        mentoria.setTitulo("Mentoria de java");
+        mentoria.setDescricao("Descrição Mentoria java");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(mentoria);
+    }
+}
